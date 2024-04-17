dedb9ca68ad4e6ef7077afc057aa3037a906bbe8
fethan <fethan.us@gmail.com>
2024-04-17 23:56:43 +0300

    pipeline first test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World! hello marask :D'));
+app.get('/', (req, res) => res.send('Hello World! hello marask :D i think this will work'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
